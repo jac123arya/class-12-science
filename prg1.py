482cd4346e79e84b4bac9c219ef930f7bccaeab8
jac123arya <125256192+jac123arya@users.noreply.github.com>
2025-08-27 12:42:14 +0530

    Add files via upload

---
+++ prg1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  1 12:21:19 2025
+
+@author: Asus
+"""
+a=7
+b=2
+print("Value of a=",a)
+print("Value of b=",b)
+add=a+b
+sub=a-b
+mulp=a*b
+divid=a/b
+remind=a%b
+expo=a**b
+fldiv=a//b
+print("Result of add=",add)
+print("Result of sub=",sub)
+print("Result of mulp=",mulp)
+print("Result of divid=",divid)
+print("Result of remind=",remind)
+print("Result of expo=",expo)
+print("Result of fldiv=",fldiv)
