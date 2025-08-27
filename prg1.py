67290ed78f9a038a6361a90b79d88bfeeb52c690
jac123arya <125256192+jac123arya@users.noreply.github.com>
2025-08-27 12:59:07 +0530

    Update prg1.py

--- prg1.py
+++ prg1.py
@@ -4,6 +4,8 @@ Created on Tue Jul  1 12:21:19 2025
 
 @author: Asus
 """
+import numpy as np
+import pandas as pd
 a=int(input("Enter the value of a:")
 b=int(input("Enter the value of b:")
 print("Value of a=",a)
@@ -22,4 +24,5 @@ print("Result of divid=",divid)
 print("Result of remind=",remind)
 print("Result of expo=",expo)
 print("Result of fldiv=",fldiv)
+
 
